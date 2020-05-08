2aa1b36939ecabf484f51b8d7f536cbdeac1fdba
Brian <brian.damian.luna@gmail.com>
2020-05-07 23:15:38 -0300

    Borrando métodos no utilizados

--- src/main/java/dds/monedero/model/Cuenta.java
+++ src/main/java/dds/monedero/model/Cuenta.java
@@ -10,29 +10,21 @@ import dds.monedero.exceptions.MontoNegativoException;
 import dds.monedero.exceptions.SaldoMenorException;
 
 public class Cuenta {
-  //Temporary Field -> El saldo de la cuenta se puede obtener fácilmente accediendo a la lista de movimientos.
-  //Se eliminó el atributo saldo y se modificó el método getSaldo. Se agregó atributo saldo incial.
   final private double saldoInicial;
-  private List<Movimiento> movimientos = new ArrayList<>();
+  final private List<Movimiento> movimientos = new ArrayList<>();
 
   public Cuenta(double montoInicial) {
     saldoInicial = montoInicial;
   }
 
-  public Cuenta() {
-    this.saldoInicial = 0;
-  }
-
-  public void setMovimientos(List<Movimiento> movimientos) {
-    this.movimientos = movimientos;
-  }
+  public Cuenta() { saldoInicial = 0; }
 
   public void poner(double cuanto) {
     if (cuanto <= 0) {
       throw new MontoNegativoException(cuanto + ": el monto a ingresar debe ser un valor positivo");
     }
 
-    if (getMovimientos().stream().filter(movimiento -> movimiento.isDeposito()).count() >= 3) {
+    if (getMovimientos().stream().filter(Movimiento::isDeposito).count() >= 3) {
       throw new MaximaCantidadDepositosException("Ya excedio los " + 3 + " depositos diarios");
     }
 
@@ -43,21 +35,19 @@ public class Cuenta {
     if (cuanto <= 0) {
       throw new MontoNegativoException(cuanto + ": el monto a ingresar debe ser un valor positivo");
     }
+
     if (getSaldo() - cuanto < 0) {
       throw new SaldoMenorException("No puede sacar mas de " + getSaldo() + " $");
     }
-    double montoExtraidoHoy = getMontoExtraidoA(LocalDate.now());
-    double limite = 1000 - montoExtraidoHoy;
+
+    double limite = 1000 - getMontoExtraidoA(LocalDate.now());
+
     if (cuanto > limite) {
       throw new MaximoExtraccionDiarioException("No puede extraer mas de $ " + 1000
           + " diarios, límite: " + limite);
     }
-    this.movimientos.add(new Movimiento(LocalDate.now(), cuanto, TipoDeMovimiento.EXTRACCION));
-  }
 
-  public void agregarMovimiento(LocalDate fecha, double cuanto, TipoDeMovimiento tipoDeMovimiento) {
-    Movimiento movimiento = new Movimiento(fecha, cuanto, tipoDeMovimiento);
-    movimientos.add(movimiento);
+    this.movimientos.add(new Movimiento(LocalDate.now(), cuanto, TipoDeMovimiento.EXTRACCION));
   }
 
   public double getMontoExtraidoA(LocalDate fecha) {
@@ -72,7 +62,6 @@ public class Cuenta {
   }
 
   public double getSaldo() {
-    return this.saldoInicial + this.movimientos.stream().mapToDouble(Movimiento::getMonto).count();
+    return saldoInicial + getMovimientos().stream().mapToDouble(Movimiento::getMonto).count();
   }
-
 }
--- src/main/java/dds/monedero/model/Movimiento.java
+++ src/main/java/dds/monedero/model/Movimiento.java
@@ -3,15 +3,9 @@ package dds.monedero.model;
 import java.time.LocalDate;
 
 public class Movimiento {
-  private LocalDate fecha;
-  //En ningún lenguaje de programación usen jamás doubles para modelar dinero en el mundo real
-  //siempre usen numeros de precision arbitraria, como BigDecimal en Java y similares
-  private double monto;
-
-  //Primitive obsession -> Como la operación a realizar al agregar un movimiento a la cuenta varía si es depósito o no,
-  //es una opción modelar el tipo de movimiento con otra abstracción.
-  //Modifiqué el boolean por un enum.
-  private TipoDeMovimiento tipoDeMovimiento;
+  final private LocalDate fecha;
+  final private double monto;
+  final private TipoDeMovimiento tipoDeMovimiento;
 
   public Movimiento(LocalDate fecha, double monto, TipoDeMovimiento tipoDeMovimiento) {
     this.fecha = fecha;
@@ -27,32 +21,7 @@ public class Movimiento {
     return fecha;
   }
 
-  public boolean fueDepositado(LocalDate fecha) {
-    return isDeposito() && esDeLaFecha(fecha);
-  }
-
-  public boolean fueExtraido(LocalDate fecha) {
-    return isExtraccion() && esDeLaFecha(fecha);
-  }
-
-  public boolean esDeLaFecha(LocalDate fecha) {
-    return this.fecha.equals(fecha);
-  }
-
   public boolean isDeposito() {
     return this.tipoDeMovimiento == TipoDeMovimiento.DEPOSITO;
-  }
-
-  public boolean isExtraccion() {
-    return this.tipoDeMovimiento == TipoDeMovimiento.EXTRACCION;
-  }
-
-  //Feature envy -> Por qué Movimiento tiene que setearle el saldo a Cuenta y agregarse en Cuenta?
-  //Se elimina el método y se modifican los métodos sacar y poner.
-
-  //Type test -> Está chequeando si el movimiento es o no un depósito para saber qué hacer.
-  //Modifiqué el switch statement por delegación.
-  public double calcularValor(Cuenta cuenta) {
-    return this.tipoDeMovimiento.calcularValor(cuenta.getSaldo(), this.monto);
   }
 }
